14553cbbcac38d261c57597f483262f9c2261eab
ishidaclar <cishidadias@gmail.com>
2026-04-05 16:42:13 -0300

    método de enviar solicitacao ajustado

--- src/Usuário.java
+++ src/Usuário.java
@@ -55,179 +55,60 @@ public class Usuário {
         System.out.println("1- Ser anônimo");
         System.out.println("2- Se identificar");
         int respostaIdentificar = scanner.nextInt();
-        if (respostaIdentificar ==1){
+        scanner.nextLine();
+        if (respostaIdentificar ==1) {
             nome = "Anonimo";
             cpf = "Anonimo";
             numeroCelular = "Anonimo";
             idade = 0;
             solicitacao.setAnonimoOuNao(true);
-
-            System.out.println("Qual a categoria da solicitação");
-            System.out.println("1-Saúde");
-            System.out.println("2-Educação");
-            System.out.println("3-Limpeza");
-            System.out.println("4-Buracos");
-            System.out.println("5-Iluminação");
-            System.out.println("6-Segurança escolar");
-            int respostaCategoria = scanner.nextInt();
-            switch (respostaCategoria){
-                String localizacaoResposta;
-                String descricaoSolicitacaoResposta;
-                case 1:
-                    solicitacao.setCategoria("Saude");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-                case 2:
-                    solicitacao.setCategoria("Educação");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-                case 3:
-                    solicitacao.setCategoria("Limpeza");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-                case 4:
-                    solicitacao.setCategoria("Buracos");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-                case 5:
-                    solicitacao.setCategoria("Iluminacao");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-
-                case 6:
-                    solicitacao.setCategoria("Segurança escolar");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-
-                default:
-                    System.out.println("Opção inválida");
-            }
-
-
-        } else if (respostaIdentificar == 2){
-            nome = nome;
-            cpf = cpf;
-            numeroCelular = numeroCelular;
-            idade = idade;
+        } else if (respostaIdentificar ==2){
             solicitacao.setAnonimoOuNao(false);
-
-            System.out.println("Qual a categoria da solicitação");
-            System.out.println("1-Saúde");
-            System.out.println("2-Educação");
-            System.out.println("3-Limpeza");
-            System.out.println("4-Buracos");
-            System.out.println("5-Iluminação");
-            System.out.println("6-Segurança escolar");
-            int respostaCategoria = scanner.nextInt();
-            switch (respostaCategoria){
-                String localizacaoResposta;
-                String descricaoSolicitacaoResposta;
-                case 1:
-                    solicitacao.setCategoria("Saude");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-                case 2:
-                    solicitacao.setCategoria("Educação");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-                case 3:
-                    solicitacao.setCategoria("Limpeza");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-                case 4:
-                    solicitacao.setCategoria("Buracos");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-                case 5:
-                    solicitacao.setCategoria("Iluminacao");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-
-                case 6:
-                    solicitacao.setCategoria("Segurança escolar");
-                    System.out.println("Qual a localização?");
-                    localizacaoResposta = scanner.nextLine();
-                    solicitacao.setLocalizacao(localizacaoResposta);
-                    System.out.println("Descreva a solicitacao:");
-                    descricaoSolicitacaoResposta = scanner.nextLine();
-                    solicitacao.setDescricao(descricaoSolicitacaoResposta);
-                    solicitacao.setStatus("Solicitação criada");
-                    break;
-
-                default:
-                    System.out.println("Opção inválida");
-            }
-
         } else {
             System.out.println("Alternativa inválida");
+            return;
         }
 
+            String localizacaoResposta;
+            String descricaoSolicitacaoResposta;
+            System.out.println("Qual a categoria da solicitação");
+            System.out.println("1-Saúde");
+            System.out.println("2-Educação");
+            System.out.println("3-Limpeza");
+            System.out.println("4-Buracos");
+            System.out.println("5-Iluminação");
+            System.out.println("6-Segurança escolar");
+            int respostaCategoria = scanner.nextInt();
+            scanner.nextLine();
+            switch (respostaCategoria) {
+                case 1:
+                    solicitacao.setCategoria("Saúde");
+                    break;
+                case 2:
+                    solicitacao.setCategoria("Educação");
+                    break;
+                case 3:
+                    solicitacao.setCategoria("Limpeza");
+                    break;
+                case 4:
+                    solicitacao.setCategoria("Buracos");
+                    break;
+                case 5:
+                    solicitacao.setCategoria("Iluminação");
+                    break;
+                case 6:
+                    solicitacao.setCategoria("Segurança escolar");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    return;
+            }
 
+        System.out.println("Qual a localização?");
+        solicitacao.setLocalizacao(scanner.nextLine());
+        System.out.println("Qual a descrição da solicitação?");
+        solicitacao.setDescricao(scanner.nextLine());
+
+        solicitacao.setStatus("Solicitacao criada");
     }
 }
