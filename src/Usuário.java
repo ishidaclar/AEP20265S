58007a4bd270858703b935217f33458666c4a7f6
ishidaclar <cishidadias@gmail.com>
2026-04-05 15:37:09 -0300

    usuário criado

---
+++ src/Usuário.java
@@ -0,0 +1,41 @@
+public class Usuário {
+
+    private String nome;
+    private String cpf;
+    private String numeroCelular;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNumeroCelular() {
+        return numeroCelular;
+    }
+
+    public void setNumeroCelular(String numeroCelular) {
+        this.numeroCelular = numeroCelular;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void
+}
