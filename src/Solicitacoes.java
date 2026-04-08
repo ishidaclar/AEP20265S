bd915cd8e89a3a19fff80d3eecb5c063dbb80bd4
ishidaclar <cishidadias@gmail.com>
2026-04-08 07:21:40 -0300

    solicitacoes métodos melhorados

--- src/Solicitacoes.java
+++ src/Solicitacoes.java
@@ -1,19 +1,24 @@
 public class Solicitacoes {
 
+    public static final String ABERTO = "Aberto";
+    public static final String TRIAGEM = "Triagem";
+    public static final String EM_EXECUCAO = "Em execução";
+    public static final String RESOLVIDO = "Resolvido";
+    public static final String ENCERRADO = "Encerrado";
     private int id;
     private String categoria;
     private String descricao;
     private String localizacao;
-    private boolean anonimoOuNao;
+    private boolean anonimoOuNao=false;
     private String status;
     private String retorno;
+    private static int contador=1;
 
-    public Solicitacoes(int id, String categoria, String descricao, String localizacao, boolean anonimoOuNao, String status) {
-        this.id = id;
+    public Solicitacoes(String categoria, String descricao, String localizacao, String status) {
+        this.id = contador++;
         this.categoria = categoria;
         this.descricao = descricao;
         this.localizacao = localizacao;
-        this.anonimoOuNao = anonimoOuNao;
         this.status = status;
     }
 
@@ -73,5 +78,33 @@ public class Solicitacoes {
         this.retorno = retorno;
     }
 
+    public void avancarStatus() {
+        if (status==null){
+            return;
+        }
+        if (status.equals(ABERTO)) {
+            status = TRIAGEM;
+        } else if (status.equals(TRIAGEM)) {
+            status = EM_EXECUCAO;
+        } else if (status.equals(EM_EXECUCAO)) {
+            status = RESOLVIDO;
+        } else if (status.equals(RESOLVIDO)) {
+            status = ENCERRADO;
+        }
+    }
+
+    public void atualizarStatusComRetorno(String novoStatus, String retorno) {
+        this.status = novoStatus;
+        this.retorno = retorno;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("ID: " + id);
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Localização: " + localizacao);
+        System.out.println("Status: " + status);
+        System.out.println("Retorno: " + retorno);
+    }
 
 }
